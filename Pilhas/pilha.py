cf82ad000c21ea37bf23d0204790cef8754926d3
PedroH183 <pedro_freires18@outlook.com>
2023-02-15 22:03:02 -0300

    pilha completa

--- Pilhas/pilha.py
+++ Pilhas/pilha.py
@@ -1,15 +1,51 @@
+import copy as cp
+
 class Pilha :
 
-    def __init__(self , dado ):
+    def __init__(self , dado = None, prox = None ):
         self.dado = dado
-        self.next = None 
-
+        self.prox = prox 
 
     def push(self, dado):
-        self.next = Pilha(dado)
+
+        new_pilha = Pilha(self.dado, self.prox)
+        self.dado = dado
+        self.prox = new_pilha
+
+        return dado
 
     def pop(self):
-        this = self 
-        this.dado = this.next 
-        
+
+        if not self :
+            return None
+
+        apagado = self.dado
+
+        if self.prox:
+            self.dado = None
+            self.prox = None
+        else:
+            self.dado = self.prox.dado 
+            self.prox = self.prox.prox
+
+
+        return print("->>" + apagado);
+    
+    def list(self):
+        aux = cp.deepcopy(self)
+
+        while ( aux != None ):
+            print( aux.dado )
+            aux = aux.prox
+
+
+nome_one = Pilha('Pedro', None)
+nome_two = Pilha('Henrique', nome_one)
+nome_three = Pilha('Barreto', nome_two)
+nome_four = Pilha('Freires', nome_three)
+
+nome_four.list()
+
+
+nome_four.pop()
 

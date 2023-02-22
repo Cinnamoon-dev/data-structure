69ad1081503f705f2a8daab8ea6f99d3e6fc3306
PedroH183 <pedro_freires18@outlook.com>
2023-02-22 11:44:31 -0300

    melhorando a estrutura do codigo

--- Filas/fila.c
+++ Filas/fila.c
@@ -1,76 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "func.h"
 
-typedef struct{
-    int dado;
-    struct celula* prox;
-}celula;
-
-typedef struct{
-    celula* head;
-    celula* tail;
-}fila;
-
-
-fila* insertData( fila* ptr, int dado )
-{
-    celula* aux = malloc(sizeof(celula));
-    aux->dado = dado;
-    aux->prox = NULL;
-
-    if( ptr->tail != NULL )
-    {
-        ptr->tail->prox = aux;
-    }
-
-    ptr->tail = aux;
-
-    if( ptr->head == NULL )
-    {
-        ptr->head = aux;
-    }
-
-    return ptr;
-}
-
-fila* createFila()
-{
-    fila* ptr = malloc(sizeof(fila));
-    
-    ptr->head = NULL;
-    ptr->tail = NULL;
-    
-    return ptr;
-}
-
-int listData( fila* ptr )
-{
-    celula* aux = ptr->head;
-
-    while( aux != NULL  )
-    {
-        printf("%d\n", aux->dado);
-        aux = aux->prox;
-    }
-
-    return 0;
-}
-
-int first_element(fila* ptr){
-    return ptr->head->dado;
-}
-
-fila* delete( fila* ptr )
-{
-    fila* new_fila = createFila();
-
-    new_fila->head = ptr->head->prox;
-    new_fila->tail = ptr->tail;
-
-    free(ptr->head);
-
-    return new_fila;
-}
 
 int main(){
     fila* fila = createFila();    

0305b60d3a1d840b9e9f73daeed6f8c241f6109a
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2023-03-15 17:53:06 -0300

    Todas as questões resolvidas

--- Filas/Python/filaPL.py
+++ Filas/Python/filaPL.py
@@ -7,20 +7,17 @@ class Fila: # index 0 = começo , index n = final
         if dado is None:
             return
         
-        if self.dado is None or self.prox is None:
+        if self.dado is None:
             self.dado = dado
             return
-        
-        filaAux = Fila(self.dado, self.prox)
-        
-        while self.prox is not None:
-            self.dado = self.prox.dado
-            self.prox = self.prox.prox
-        
-        self.prox = Fila(dado, None)
 
-        self.dado = filaAux.dado
-        self.prox = filaAux.prox
+        f = Fila(dado, None)
+
+        filaAux = self #copia
+        while filaAux.prox is not None:
+            filaAux = filaAux.prox
+
+        filaAux.prox = f
 
     def pop(self): # Sai do começo
         if self.dado is None:
@@ -37,6 +34,21 @@ class Fila: # index 0 = começo , index n = final
         self.prox = self.prox.prox
 
         return dado
+
+    def print(self):
+        array = self.toArray([])
+        print(array)
+
+    def toArray(self, array=[]):
+        if self is None:
+            return
+
+        array.append(self.dado)
+
+        if self.prox is not None and self.prox.dado is not None:
+            self.prox.toArray(array)
+
+        return array
 
     #Q4
     def firstElementar(self):
@@ -100,3 +112,526 @@ class Fila: # index 0 = começo , index n = final
             filaAux.prox = filaAux.prox.prox
         
         return filaAux.dado
+
+    #Q8
+    def getValueByIndexElementar(self, index):
+        if self.dado is None:
+            return None
+        
+        filaAux = Fila(self.dado, self.prox)
+        counter = 0
+
+        while filaAux.dado is not None:
+            value = filaAux.pop()
+            if counter == index:
+                return value
+            counter += 1
+        
+        return None
+    
+    def getValueByIndexDiverso(self, index):
+        if self.dado is None:
+            return None
+        
+        filaAux = Fila(self.dado, self.prox)
+        counter = 0
+
+        while filaAux.dado is not None:
+            value = filaAux.dado
+            filaAux.dado = filaAux.prox.dado
+            filaAux.prox = filaAux.prox.prox
+            if counter == index:
+                return value
+            counter += 1
+        
+        return None
+    
+    #Q9
+    def getIndexByValueElementar(self, value=None):
+        if value == None:
+            return None
+
+        filaAux = Fila(self.dado, self.prox)
+        found = False
+        counter = 0
+
+        while filaAux is not None:
+            aux = filaAux.pop()
+            if aux == value:
+                found = True
+                break
+            counter += 1
+        
+        if found == True:
+            return counter
+        
+        return None
+    
+    def getIndexByValueDiverso(self, value=None):
+        if value == None:
+            return None
+
+        filaAux = Fila(self.dado, self.prox)
+        found = False
+        counter = 0
+
+        while filaAux is not None:
+            aux = filaAux.dado
+            filaAux.dado = filaAux.prox.dado
+            filaAux.prox = filaAux.prox.prox
+            if aux == value:
+                found = True
+                break
+            counter += 1
+        
+        if found == True:
+            return counter
+        
+        return None
+    
+    #Q10
+    def getAllIndexByValueElementar(self, value):
+        if value == None:
+            return []
+
+        filaAux = Fila(self.dado, self.prox)
+        indexes = []
+        counter = 0
+
+        while filaAux.dado is not None:
+            aux = filaAux.pop()
+            if aux == value:
+                indexes.append(counter)
+            counter += 1
+        
+        return indexes
+    
+    def getAllIndexByValueDiverso(self, value):
+        if value == None:
+            return []
+
+        filaAux = Fila(self.dado, self.prox)
+        indexes = []
+        counter = 0
+
+        while filaAux.dado is not None:
+            aux = filaAux.dado
+            filaAux.dado = filaAux.prox.dado
+            filaAux.prox = filaAux.prox.prox
+            if aux == value:
+                indexes.append(counter)
+            counter += 1
+        
+        return indexes
+
+    #Q11
+    def getValuesByIndexsElementar(self, indexes=None):
+        if indexes is None:
+            return []
+
+        filaAux = Fila(self.dado, self.prox)
+        values = []
+        counter = 0
+
+        while filaAux.dado is not None:
+            dado = filaAux.pop()
+            if compareEqualsValueWithListOfValues(counter, indexes):
+                values.append(dado)
+            counter += 1
+        
+        return values
+
+    def getValuesByIndexsDiverso(self, indexes=None):
+        if indexes is None:
+            return []
+
+        filaAux = Fila(self.dado, self.prox)
+        values = []
+        counter = 0
+
+        while filaAux.dado is not None:
+            dado = filaAux.dado
+            filaAux.dado = filaAux.prox.dado
+            filaAux.prox = filaAux.prox.prox
+            if compareEqualsValueWithListOfValues(counter, indexes):
+                values.append(dado)
+            counter += 1
+        
+        return values
+    
+    #Q12
+    def getValuesBySliceElementar(self, begin: int, end: int) -> list: 
+        queueSize = self.lengthElementar()
+        if queueSize < (end + 1) or queueSize < (begin + 1):
+            print("Slice out of range")
+            return None
+
+        filaAux = Fila(self.dado, self.prox)     
+        counter = 0
+        slice = []
+        
+        while counter != begin:
+            filaAux.pop()
+            counter += 1
+        
+        while counter != (end + 1):
+            value = filaAux.pop()
+            slice.append(value)
+            counter += 1
+        
+        return slice
+
+    def getValuesBySliceDiverso(self, begin: int, end: int) -> list:
+        queueSize = self.lengthElementar
+        if queueSize < (end + 1) or queueSize < (begin + 1):
+            print("Slice out of range")
+            return None
+
+        filaAux = Fila(self.dado, self.prox)
+        counter = 0
+        slice = []
+        
+        while counter != begin:
+            filaAux.dado = filaAux.prox.dado
+            filaAux.prox - filaAux.prox.prox
+            counter += 1
+        
+        while counter != (end + 1):
+            value = filaAux.dado
+            filaAux.dado = filaAux.prox.dado
+            filaAux.prox = filaAux.prox.prox
+            slice.append(value)
+            counter += 1
+        
+        return slice
+    
+    #Q13
+    def removeAllElementar(self):
+        while self.dado is not None:
+            self.pop()
+    
+    def removeAllDiverso(self):
+        self.dado = None
+        self.prox = None
+    
+    #Q14
+    def removeByIndexElementar(self, index):
+        filaAux = Fila(self.dado, self.prox)
+        counter = -1 # Para contar de 0 até n - 1, como indices
+
+        while filaAux.dado is not None:
+            filaAux.pop()
+            counter += 1
+        
+        filaAux = Fila(self.dado, self.prox)
+        filaAux2 = Fila()
+
+        while counter != index:
+            value = self.pop()
+            filaAux2.push(value) 
+            counter -= 1
+        
+        self.pop()
+
+        while filaAux2.dado is not None:
+            value = filaAux2.pop()
+            self.push(value)
+
+    def removeByIndexDiverso(self, index):
+        filaAux = Fila(self.dado, self.prox)
+        counter = -1 # Para contar de 0 até n - 1, como indices
+
+        while filaAux is not None:
+            filaAux.dado = filaAux.prox.dado
+            filaAux.prox = filaAux.prox.prox
+            counter += 1
+        
+        filaAux = Fila(self.dado, self.prox)
+        filaAux2 = Fila()
+
+        while counter != index:
+            value = self.dado
+            self.dado = self.prox.dado
+            self.prox = self.prox.prox
+            filaAux2.push(value) 
+            counter -= 1
+        
+        self.pop()
+
+        while filaAux2.dado is not None:
+            value = filaAux2.dado
+            filaAux2.dado = filaAux2.prox.dado
+            filaAux2.prox = filaAux2.prox.prox
+            self.push(value)
+
+    #Q15
+    def removeByValueElementar(self, value=None):
+        if value is None:
+            return
+        
+        filaAux = Fila()
+
+        while self.dado != value and self.dado is not None:
+            filaAux.push(self.pop())
+        
+        self.pop()
+
+        while filaAux.dado is not None:
+            self.push(filaAux.pop())
+    
+    def removeByValueDiverso(self, value=None):
+        if value is None:
+            return
+        
+        filaAux = Fila()
+
+        while self.dado != value and self.dado is not None:
+            filaAux.push(self.dado)
+            self.dado = self.prox.dado
+            self.prox = self.prox.prox
+        
+        self.pop()
+
+        while filaAux.dado is not None:
+            self.push(filaAux.dado)
+            filaAux.dado = filaAux.prox.dado
+            filaAux.prox = filaAux.prox.prox
+    
+    #Q16
+    def removeAllByValueElementar(self, value=None):
+        if value is None:
+            return
+
+        filaAux = Fila()
+        while self.dado is not None:
+            if self.dado != value:
+                filaAux.push(self.pop())
+                continue
+            self.pop()
+
+        while filaAux.dado is not None:
+            self.push(filaAux.pop())
+    
+    def removeAllByValueDiverso(self, value=None):
+        if value is None:
+            return
+
+        filaAux = Fila()
+        while self.dado is not None:
+            if self.dado != value:
+                filaAux.push(self.dado)
+                self.dado = self.prox.dado
+                self.prox = self.prox.prox
+                continue
+            self.dado = self.prox.dado
+            self.prox = self.prox.prox
+
+        while filaAux.dado is not None:
+            self.push(filaAux.dado)
+            filaAux.dado = filaAux.prox.dado
+            filaAux.prox = filaAux.prox.prox
+
+    #Q17
+    def removeAllByIndexesElementar(self, indexes: list):
+        if indexes is None or len(indexes) == 0:
+            return
+        
+        filaAux = Fila()
+        counter = 0
+        length = self.lengthElementar()
+
+        while self.dado is not None:
+            filaAux.push(self.pop())
+        
+        while counter < length:
+            if compareEqualsValueWithListOfValues(counter, indexes):
+                filaAux.pop()
+                counter += 1
+                continue
+            self.push(filaAux.pop())
+            counter += 1
+
+    def removeAllByIndexesDiverso(self, indexes: list):
+        if indexes is None or len(indexes) == 0:
+            return
+        
+        values= []
+
+        for index in indexes:
+            values.append(self.getValueByIndexElementar(index))
+        
+        for value in values:
+            self.removeByValueElementar(value)
+
+    #Q18
+    def removeAllBySliceElementar(self, start: int, end: int):
+        queueSize = self.lengthElementar()
+        if queueSize < (end + 1) or queueSize < (start + 1):
+            print("Slice out of range")
+            return None
+
+        filaAux = Fila(self.dado, self.prox)
+        counter = 0
+
+        while self.dado is not None:
+            self.pop()
+        
+        while counter != start: # [1, 2, 3, 4, 5] e (1, 3)
+            self.push(filaAux.pop())
+            counter += 1
+        
+        while counter != (end + 1):
+            filaAux.pop()
+            counter += 1
+        
+        while filaAux.dado is not None:
+            self.push(filaAux.pop())
+    
+    def removeAllBySliceDiverso(self, start: int, end: int):
+        queueSize = self.lengthDiverso()
+        if queueSize < (end + 1) or queueSize < (start + 1):
+            print("Slice out of range")
+            return None
+
+        filaAux = Fila(self.dado, self.prox)
+        counter = 0
+        
+        while self.dado is not None:
+            self.pop()
+
+        while counter != start: 
+            self.push(filaAux.dado)
+            filaAux.dado = filaAux.prox.dado
+            filaAux.prox = filaAux.prox.prox
+            counter += 1
+        
+        while counter != (end + 1):
+            filaAux.dado = filaAux.prox.dado
+            filaAux.prox = filaAux.prox.prox
+            counter += 1
+        
+        while filaAux.dado is not None:
+            self.push(filaAux.dado)
+            filaAux.dado = filaAux.prox.dado
+            filaAux.prox = filaAux.prox.prox
+
+    #Q19
+    def setValueIndexElementar(self, index, value):
+        queueSize = self.lengthElementar()
+        if index > queueSize:
+            print("Index out of bounds")
+            return
+        
+        filaAux = Fila(self.dado, self.prox)
+        counter = 0
+
+        while self.dado is not None:
+            self.pop()
+        
+        while filaAux.dado is not None:
+            if counter == index:
+                filaAux.pop()
+                self.push(value)
+                counter += 1
+                continue
+
+            self.push(filaAux.pop())
+            counter += 1
+    
+    def setValueIndexDiverso(self, index, value):
+        queueSize = self.lengthDiverso()
+        if index > queueSize:
+            print("Index out of bounds")
+            return
+        
+        filaAux = Fila(self.dado, self.prox)
+        counter = 0
+
+        while self.dado is not None:
+            self.pop()
+
+        while filaAux.dado is not None:
+            if counter == index:
+                filaAux.dado = filaAux.prox.dado
+                filaAux.prox = filaAux.prox.prox
+                self.push(value)
+                counter += 1
+                continue
+
+            self.push(filaAux.dado)
+            filaAux.dado = filaAux.prox.dado
+            filaAux.prox = filaAux.prox.prox
+            counter += 1
+
+    #Q20
+    def setValuesInIndexesElementar(self, indexes: list, elements: list):
+        queueSize = self.lengthElementar()
+        if compareValueWithLessThanListOfValues(queueSize, indexes):
+            print("One of the indexes is out of bounds!")
+            return
+
+        filaAux = Fila(self.dado, self.prox)
+        counter = 0
+
+        while self.dado is not None:
+            self.pop()
+        
+        while filaAux.dado is not None:
+            if compareEqualsValueWithListOfValues(counter, indexes):
+                filaAux.pop()
+                self.push(elements[indexes.index(counter)])
+                counter += 1
+            
+            self.push(filaAux.pop())
+            counter += 1
+
+    def setValuesInIndexesDiverso(self, indexes: list, elements: list):
+        queueSize = self.lengthDiverso()
+        if compareValueWithLessThanListOfValues(queueSize, indexes):
+            print("One of the indexes is out of bounds!")
+            return
+
+        filaAux = Fila(self.dado, self.prox)
+        counter = 0
+
+        while self.dado is not None:
+            self.dado = self.prox.dado
+            self.prox = self.prox.prox
+        
+        while filaAux.dado is not None:
+            if compareEqualsValueWithListOfValues(counter, indexes):
+                filaAux.dado = filaAux.prox.dado
+                filaAux.prox = filaAux.prox.prox
+                self.push(elements[indexes.index(counter)])
+                counter += 1
+            
+            self.push(filaAux.dado)
+            filaAux.dado = filaAux.prox.dado
+            filaAux.prox = filaAux.prox.prox
+            counter += 1
+
+# Auxiliary Functions
+def compareEqualsValueWithListOfValues(value, listOfValues: list):
+    for index in listOfValues:
+        if value == index:
+            return True
+    
+    return False
+
+def compareValueWithLessThanListOfValues(value, listOfValues: list):
+    for index in listOfValues:
+        if value < index:
+            return True
+    
+    return False
+
+
+test = Fila()
+test.push(1)
+test.push(2)
+test.push(3)
+test.push(4)
+test.push(5)
+test.print()
+test.removeByIndexElementar(0)
+test.print()

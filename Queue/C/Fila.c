aacfe0b34084d4f384d2000ea021b1fec44ced56
PedroH183 <pedro_freires18@outlook.com>
2023-04-06 21:19:10 -0300

    incrementing a prototype of a queue in C

--- Queue/C/Fila.c
+++ Queue/C/Fila.c
@@ -8,7 +8,7 @@ typedef struct _celula {
 
 celula* createCelula( int dado, celula* prox )
 {
-    celula* ptr = malloc(sizeof(celula));
+    celula* ptr = malloc( sizeof( celula ) );
     ptr->dado = dado;
     ptr->prox = prox;
 
@@ -30,9 +30,9 @@ void insertData( celula** fila_celulas, int dado)
     return ;
 }
 
-void listData( celula* fila_pointer )
+void listData( celula* fila_celulas )
 {
-    celula* aux = fila_pointer;
+    celula* aux = fila_celulas;
 
     while( aux != NULL )
     {
@@ -40,10 +40,10 @@ void listData( celula* fila_pointer )
         aux = aux->prox;
     }
 
-    return;
+    return ;
 }
 
-int delete(celula** fila_pointer)
+int deleteData(celula** fila_pointer)
 {
     int aux = (*fila_pointer)->dado;
     celula* first_celula = (*fila_pointer);
@@ -55,20 +55,137 @@ int delete(celula** fila_pointer)
     return aux;
 }
 
+int peekElement( celula* fila_pointer )
+{
+    // return the first element of my data structure 
+    return fila_pointer->dado;
+}
+
+int isEmpty( celula* fila_pointer )
+{
+    return !(fila_pointer != NULL);
+}
+
+int lengthFila( celula* fila_pointer )
+{
+    if (fila_pointer == NULL){
+        return -1;
+    }
+
+    int counter = 0;
+
+    while( fila_pointer != NULL){
+        counter++;
+        fila_pointer = fila_pointer->prox;
+    }
+
+    return counter;
+}
+
+int lastElement( celula* fila_pointer ){
+    
+    if(fila_pointer == NULL){
+        return -1;
+    }
+
+    celula* aux_pointer = fila_pointer;
+    
+    while( aux_pointer->prox != NULL ){
+        aux_pointer = aux_pointer->prox;
+    }
+
+    int last_element = aux_pointer->dado;
+
+    return last_element;
+}
+
+int getValueByIndex( celula* fila_pointer, int index ){
+
+    if( (fila_pointer == NULL) || index > lengthFila(fila_pointer) ){
+        return -1;
+    }
+
+    int counter = 0;
+    celula* aux_pointer = fila_pointer;
+
+    while( aux_pointer != NULL ){
+
+        if( index == counter ){
+            return aux_pointer->dado;
+        }
+
+        counter += 1;
+        aux_pointer = aux_pointer->prox;
+    }
+    return -1;
+}
+
+int getIndexByValue( celula* fila_pointer, int dado ){
+
+    if( fila_pointer == NULL ){
+        return -1;
+    }
+
+    int counter = 0;
+    celula* aux_pointer = fila_pointer;
+
+    while( aux_pointer != NULL ){
+        if( aux_pointer->dado == dado ){
+            return counter;
+        }
+        aux_pointer = aux_pointer->prox;
+    }
+    return -1;
+}
+
+void getAllIndexByValue( celula* fila_pointer, int** result,int dado ){
+    
+    if( fila_pointer == NULL ){
+        return ;
+    }
+
+    int length_array = 1;
+    int index_array = 0;
+    (*result) = malloc( length_array * sizeof(int) );
+
+    int counter = 0;
+    celula* aux_pointer = fila_pointer;
+
+    while( aux_pointer != NULL )
+    {    
+        if( aux_pointer->dado == dado )
+        {
+            length_array++;
+            (*result) = realloc( (*result) , length_array * sizeof(int));
+            *( (*result) + index_array ) = counter;
+            index_array++;
+        }
+        counter += 1;
+        aux_pointer = aux_pointer->prox;
+    }
+}
+
 int main()
 {
     celula* f_ptr = createCelula(100, NULL);
+    int* indexs = NULL;
 
     insertData( &f_ptr, 120 );
     insertData( &f_ptr, 140 );
     insertData( &f_ptr, 160 );
-
-    listData( f_ptr );
-
-    printf(">>%d, deleted\n", delete(&f_ptr));
-    printf(">>%d, deleted\n", delete(&f_ptr));
+    insertData( &f_ptr, 140 );
 
     listData(f_ptr);
+    printf(">>%d, length of my queue\n", lengthFila(f_ptr));
+    printf(">>%d, last element of my queue\n", lastElement(f_ptr));
+    printf(">>%d, get element by index \n", getValueByIndex(f_ptr, 2));
+
+    getAllIndexByValue(f_ptr, &indexs, 140);
+
+    for(int i = 0; i < 2 ; ++i)
+    {
+        printf("data in index %d, >> %d\n", i, *(indexs + i));
+    }
 
     return 0;
 }
